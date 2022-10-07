3d3de96616a724f671acca7ad8c441c38fbf8504
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-07 14:08:48 +0530

    Q&R

---
+++ QuotientRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    public static class QuotientRemainder
+    {
+        public static void CheckQuotientRemainder()
+        {
+            int dividend = 50, divisor = 8;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Dividend: {0} Divisor: {1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);        
+         }
+    }
+}
