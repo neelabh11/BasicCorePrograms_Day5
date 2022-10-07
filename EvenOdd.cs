3b853b37efe493eb04309ba6d19d1f94bbf596f7
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-07 17:58:22 +0530

    even or odd

---
+++ EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    public static class EvenOdd
+    {
+        public static void CheckEvenOrOdd()
+        {
+            int i;
+            Console.WriteLine("Enter a number: ");
+            i = Convert.ToInt32(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i + " is even");
+            }
+            else
+            {
+                Console.WriteLine(i + " is odd");
+            }
+        }
+    }
+}
