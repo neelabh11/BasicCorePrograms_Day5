291f6bd360a9ef8d96fd288bffba103a845654bd
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-07 17:25:56 +0530

    swapping

---
+++ SwapTwoNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    public static class SwapTwoNumbers
+    {
+        public static void SwapNumber()
+        {
+            int a = 5, b = 10;
+
+            Console.WriteLine("Before swap a = " + a + " b = " + b);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("After swap a = " + a + " b = " + b);
+        }
+    }
+}
